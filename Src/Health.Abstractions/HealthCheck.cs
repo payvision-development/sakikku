4986ccb70c6bf469adc678b3cd93ced53c978613
Oscar Fernandez <o.fernandez@payvision.com>
2019-05-17 13:01:52 +0200

    Health policy abstraction definition

---
+++ Src/Health.Abstractions/HealthCheck.cs
@@ -0,0 +1,29 @@
+﻿namespace Payvision.Diagnostics.Health
+{
+    /// <summary>
+    /// Contains the result of a health policy check.
+    /// </summary>
+    public sealed class HealthCheck
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HealthCheckResult"/> struct.
+        /// </summary>
+        /// <param name="status">The status.</param>
+        /// <param name="message">The message.</param>
+        public HealthCheck(HealthStatus status, string message)
+        {
+            this.Status = status;
+            this.Message = message;
+        }
+
+        /// <summary>
+        /// Gets the status of the checked component.
+        /// </summary>
+        public HealthStatus Status { get; }
+
+        /// <summary>
+        /// Gets a readable message that describes the health check result.
+        /// </summary>
+        public string Message { get; }
+    }
+}
---
+++ Src/Health.Abstractions/IHealthPolicy.cs
@@ -0,0 +1,20 @@
+﻿namespace Payvision.Diagnostics.Health
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines a health check policy that can be used to check the status of
+    /// either a part or a dependency of the checking application.
+    /// </summary>
+    public interface IHealthPolicy
+    {
+        /// <summary>
+        /// Runs the health check.
+        /// </summary>
+        /// <remarks>The health check service will catch any thrown exception projecting to an unhealthy result.</remarks>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> of the asynchronous operation.</param>
+        /// <returns>The result of the health check.</returns>
+        Task<HealthCheck> CheckAsync(CancellationToken cancellationToken);
+    }
+}
